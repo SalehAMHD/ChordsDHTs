85b17772ca611e2792fb7506ed7fb5533df7783d
RedCheb17 <2005redhouanchebbi@gmail.com>
2025-12-16 23:24:06 +0100

    v2

--- chord_sim/lib/chord_sim/node.ex
+++ chord_sim/lib/chord_sim/node.ex
@@ -1,68 +1,60 @@
-defmodule ChordSim.Node do
-  @moduledoc """
-  Minimal GenServer skeleton for a Chord node. Implements basic API stubs
-  to be extended: join, find_successor, put/get, stabilize, fix_fingers, etc.
-  """
-
+﻿defmodule ChordSim.Node do
   use GenServer
 
-  @default_m 32
+  @m 32
 
   ## Public API
   def start_link(opts) when is_list(opts) do
     id = Keyword.fetch!(opts, :id)
-    GenServer.start_link(__MODULE__, %{id: id, opts: opts}, name: via_tuple(id))
+    GenServer.start_link(__MODULE__, id, name: via(id))
   end
 
-  def child_spec(opts) do
-    id = Keyword.fetch!(opts, :id)
-    %{
-      id: {:chord_node, id},
-      start: {__MODULE__, :start_link, [opts]},
-      restart: :transient
-    }
-  end
+  def via(id), do: {:via, Registry, {ChordSim.NodeRegistry, id}}
 
-  defp via_tuple(id), do: {:via, Registry, {ChordSim.NodeRegistry, id}}
+  def join(id, known_id \\ nil), do: GenServer.call(via(id), {:join, known_id})
+  def find_successor(id, key), do: GenServer.call(via(id), {:find_successor, key})
+  def put(id, key, value), do: GenServer.call(via(id), {:put, key, value})
+  def get(id, key), do: GenServer.call(via(id), {:get, key})
+  def info(id), do: GenServer.call(via(id), :info)
 
-  # API helpers that call the GenServer by Registry name
-  def join(id, known_node \\ nil), do: GenServer.call(via_tuple(id), {:join, known_node})
-  def leave(id), do: GenServer.cast(via_tuple(id), :leave)
-  def find_successor(id, key_id), do: GenServer.call(via_tuple(id), {:find_successor, key_id})
-  def put(id, key, value), do: GenServer.call(via_tuple(id), {:put, key, value})
-  def get(id, key), do: GenServer.call(via_tuple(id), {:get, key})
-  def info(id), do: GenServer.call(via_tuple(id), :info)
-
-  ## GenServer callbacks
-  def init(%{id: id} = _init_arg) do
-    table_name = String.to_atom("node_#{id}_table")
-    # ensure unique named table per node
-    if :ets.whereis(table_name) != :undefined do
-      :ets.delete(table_name)
-    end
-
-    :ets.new(table_name, [:set, :protected, :named_table])
+  ## Callbacks
+  @impl true
+  def init(id) do
+    table = :ets.new(String.to_atom("node_#{id}_table"), [:set, :protected, :named_table])
 
     state = %{
       id: id,
-      m: @default_m,
-      successor: nil,
+      m: @m,
       predecessor: nil,
-      fingers: [],
-      table: table_name
+      successor: id,
+      table: table
     }
 
     {:ok, state}
   end
 
-  def handle_call({:join, _known_node}, _from, state) do
-    # stub: implement join logic
-    {:reply, :ok, state}
+  @impl true
+  def handle_call({:join, nil}, _from, state) do
+    # Premier noeud : boucle sur lui-même
+    {:reply, :ok, %{state | successor: state.id, predecessor: state.id}}
   end
 
-  def handle_call({:find_successor, _key_id}, _from, state) do
-    # stub: real implementation should route in the ring
-    {:reply, {:ok, state.successor || :not_found}, state}
+  def handle_call({:join, known_id}, _from, state) do
+    {:ok, succ} = GenServer.call(via(known_id), {:find_successor, state.id})
+    {:reply, :ok, %{state | successor: succ}}
+  end
+
+  def handle_call({:find_successor, key}, _from, state) do
+    cond do
+      state.successor == state.id ->
+        {:reply, {:ok, state.id}, state}
+
+      in_interval?(key, state.id, state.successor) ->
+        {:reply, {:ok, state.successor}, state}
+
+      true ->
+        GenServer.call(via(state.successor), {:find_successor, key})
+    end
   end
 
   def handle_call({:put, key, value}, _from, state) do
@@ -78,16 +70,20 @@ defmodule ChordSim.Node do
   end
 
   def handle_call(:info, _from, state) do
-    keys = :ets.tab2list(state.table)
-    {:reply, %{id: state.id, successor: state.successor, predecessor: state.predecessor, keys: keys}, state}
+    {:reply, %{id: state.id, succ: state.successor, pred: state.predecessor}, state}
   end
 
-  def handle_cast(:leave, state) do
-    {:stop, :normal, state}
-  end
-
+  @impl true
   def terminate(_reason, state) do
     if is_atom(state.table), do: :ets.delete(state.table)
     :ok
+  end
+
+  defp in_interval?(key, start_id, end_id) do
+    cond do
+      start_id < end_id -> key > start_id and key <= end_id
+      start_id > end_id -> key > start_id or key <= end_id
+      true -> true
+    end
   end
 end
